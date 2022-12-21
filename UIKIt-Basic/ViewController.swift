f91596c219779bc02a197edfc1e2776657ef1687
Nadia <ivashchenko.nadia@gmail.com>
2022-12-21 19:02:16 +0200

    Initial Commit

---
+++ UIKIt-Basic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKIt-Basic
+//
+//  Created by Students on 21.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
